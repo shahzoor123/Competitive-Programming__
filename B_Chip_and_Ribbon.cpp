20c64784b30914c39dcb42b82a08b6e27bd4b4ad
Shahzoor Khan <shahzoork@gmail.com>
2026-06-29 00:05:20 +0500

    Solved one problem called B. Chip and Ribbon

---
+++ B_Chip_and_Ribbon.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define vi vector<int>
+#define vl vector<long long>
+#define endl "\n"
+#define nline cout<<endl
+#define f(i, n) for (ll i = 0; i < n; ++i)
+
+template <typename T>
+void prnt(const T &val) {
+    if (std::is_arithmetic<T>::value || std::is_same<T, string>::value) {
+        cout << val << '\n';
+    } else {
+        for (const auto &x : val)
+            cout << x << ' ';
+        cout << '\n';
+    }
+}
+
+// Problem Statement
+/*
+  
+*/
+
+// Small Observations
+/*
+  
+*/
+
+// Claims on Algo
+/*
+  
+*/
+void solve() {
+
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    for(int &x : a) cin >> x;
+
+    ll sum = 0;
+
+    for(int i=0;i<n;i++){
+        if(i == 0){
+            sum += a[i] - 1;
+        }
+        else if (a[i] > a[i-1]){
+            sum += a[i] - a[i-1];
+        }
+    }
+
+    cout << sum << endl;
+
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    while(t--) solve();
+
+    return 0;
+}
